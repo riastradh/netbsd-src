4454314a82454f95cf2bad2b32e212c2e068bab6
Maya Rashish <maya@NetBSD.org>
2020-03-28 18:41:01 +0300

    Make ourselves less dependent on drmP.h, removed upstream.
    This causes some trouble as CONFIG_* lines might not be appropriately
    defined. A few declarations remain in drmP.h so it's not gone.

--- sys/external/bsd/drm2/drm/drm_cdevsw.c
+++ sys/external/bsd/drm2/drm/drm_cdevsw.c
@@ -60,6 +60,7 @@ __KERNEL_RCSID(0, "$NetBSD: drm_cdevsw.c,v 1.16 2021/09/26 01:16:10 thorpej Exp 
 #include <drm/drmP.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_file.h>
+#include <drm/drm_irq.h>
 #include <drm/drm_legacy.h>
 
 #include "../dist/drm/drm_internal.h"
