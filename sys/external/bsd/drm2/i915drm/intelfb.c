ca882e2814bcdee9d510f471212f2b713a3ba95d
Taylor R Campbell <riastradh@NetBSD.org>
2020-04-12 05:55:08 +0000

    Remove last users of drmP.h.

--- sys/external/bsd/drm2/i915drm/intelfb.c
+++ sys/external/bsd/drm2/i915drm/intelfb.c
@@ -36,7 +36,7 @@ __KERNEL_RCSID(0, "$NetBSD: intelfb.c,v 1.18 2021/06/12 12:15:43 riastradh Exp $
 #include <sys/bus.h>
 #include <sys/device.h>
 
-#include <drm/drmP.h>
+#include <drm/drm_device.h>
 #include <drm/drmfb.h>
 #include <drm/drmfb_pci.h>
 
