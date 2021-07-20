5d12aeeb9201b183a7ab3f17bce3d7b29c9a58c4
Taylor R Campbell <riastradh@NetBSD.org>
2021-07-20 19:32:56 +0000

    i915: Draft intel ggtt partial page view.

--- sys/external/bsd/drm2/dist/drm/i915/gem/i915_gem_mman.c
+++ sys/external/bsd/drm2/dist/drm/i915/gem/i915_gem_mman.c
@@ -434,9 +434,6 @@ static vm_fault_t vm_fault_gtt(struct vm_fault *vmf)
 				       PIN_MAPPABLE |
 				       PIN_NONBLOCK /* NOWARN */ |
 				       PIN_NOEVICT);
-#ifdef __NetBSD__		/* XXX i915 partial views */
-	(void)compute_partial_view;
-#else
 	if (IS_ERR(vma)) {
 		/* Use a partial view if it is bigger than available space */
 		struct i915_ggtt_view view =
@@ -462,7 +459,6 @@ static vm_fault_t vm_fault_gtt(struct vm_fault *vmf)
 		/* The entire mappable GGTT is pinned? Unexpected! */
 		GEM_BUG_ON(vma == ERR_PTR(-ENOSPC));
 	}
-#endif
 	if (IS_ERR(vma)) {
 		ret = PTR_ERR(vma);
 		goto err_reset;
