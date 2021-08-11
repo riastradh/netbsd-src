06df628cf67a8c6a5158fe2a6634eb61f8438d04
Taylor R Campbell <riastradh@NetBSD.org>
2021-08-11 18:06:18 +0000

    radeon: Fix dma_addressing_limited call.

--- sys/external/bsd/drm2/dist/drm/radeon/radeon_ttm.c
+++ sys/external/bsd/drm2/dist/drm/radeon/radeon_ttm.c
@@ -988,7 +988,7 @@ int radeon_ttm_init(struct radeon_device *rdev)
 			       rdev->ddev->anon_inode->i_mapping,
 #endif
 			       rdev->ddev->vma_offset_manager,
-			       dma_addressing_limited(&rdev->pdev->pd_dev));
+			       dma_addressing_limited(pci_dev_dev(rdev->pdev)));
 	if (r) {
 		DRM_ERROR("failed initializing buffer object driver(%d).\n", r);
 		return r;
