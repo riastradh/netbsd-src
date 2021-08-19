9e8677fb8ec7de0b5b69dfb032a332b7b84a8a14
Taylor R Campbell <riastradh@NetBSD.org>
2021-08-19 13:05:45 +0000

    ksyms: Use pserialize(9) for kernel access to ksyms.
    This makes it available in interrupt context, e.g. for printing
    messages with kernel symbol names for return addresses as drm wants
    to do.
    
    (cherry picked from commit e7e7801c8eaebd53ebc83f091e46a3ef0c86db96)

--- sys/sys/ksyms.h
+++ sys/sys/ksyms.h
@@ -39,6 +39,10 @@
 #include <sys/ioccom.h>
 #include <sys/queue.h>
 
+#ifdef _KERNEL
+#include <sys/pslist.h>
+#endif
+
 struct ksyms_symtab {
 	TAILQ_ENTRY(ksyms_symtab) sd_queue; /* All active tables */
 	const char *sd_name;	/* Name of this table */
@@ -55,6 +59,10 @@ struct ksyms_symtab {
 	int sd_ctfsize;		/* Size in bytes of CTF contents */
 	uint32_t *sd_nmap;	/* Name map for sorted symbols */
 	int sd_nmapsize;	/* Total span of map */
+
+#ifdef _KERNEL
+	struct pslist_entry sd_pslist;
+#endif
 };
 
 /*
