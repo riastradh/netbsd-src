coypoop/drm

drm: Work around bus_space_read_8 on LP32.

sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_device.c
@@ -483,8 +483,24 @@ u64 amdgpu_mm_rdoorbell64(struct amdgpu_device *adev, u32 index)
 {
 	if (index < adev->doorbell.num_doorbells) {
 #ifdef __NetBSD__
+#ifdef _LP64
 		return bus_space_read_8(adev->doorbell.bst, adev->doorbell.bsh,
 		    8*index);
+#else
+		uint64_t lo, hi;
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+		lo = bus_space_read_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index);
+		hi = bus_space_read_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index + 4);
+#else
+		hi = bus_space_read_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index);
+		lo = bus_space_read_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index + 4);
+#endif
+		return lo | (hi << 32);
+#endif
 #else
 		return atomic64_read((atomic64_t *)(adev->doorbell.ptr + index));
 #endif
@@ -508,8 +524,22 @@ void amdgpu_mm_wdoorbell64(struct amdgpu_device *adev, u32 index, u64 v)
 {
 	if (index < adev->doorbell.num_doorbells) {
 #ifdef __NetBSD__
+#ifdef _LP64
 		bus_space_write_8(adev->doorbell.bst, adev->doorbell.bsh,
 		    8*index, v);
+#else
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+		bus_space_write_4(adev->doorbell.bst, adev->doorbell.hs,
+		    8*index, v & 0xffffffffU);
+		bus_space_write_4(adev->doorbell.bst, adev->doorbell.hs,
+		    8*index + 4, v >> 32);
+#else
+		bus_space_write_4(adev->doorbell.bst, adev->doorbell.hs,
+		    8*index, v >> 32);
+		bus_space_write_4(adev->doorbell.bst, adev->doorbell.hs,
+		    8*index + 4, v & 0xffffffffU);
+#endif
+#endif
 #else
 		atomic64_set((atomic64_t *)(adev->doorbell.ptr + index), v);
 #endif

riastradh/netbsd-src

sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_device.c
@@ -483,8 +483,24 @@ u64 amdgpu_mm_rdoorbell64(struct amdgpu_device *adev, u32 index)
 {
 	if (index < adev->doorbell.num_doorbells) {
 #ifdef __NetBSD__
+#ifdef _LP64
 		return bus_space_read_8(adev->doorbell.bst, adev->doorbell.bsh,
 		    8*index);
+#else
+		uint64_t lo, hi;
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+		lo = bus_space_read_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index);
+		hi = bus_space_read_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index + 4);
+#else
+		hi = bus_space_read_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index);
+		lo = bus_space_read_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index + 4);
+#endif
+		return lo | (hi << 32);
+#endif
 #else
 		return atomic64_read((atomic64_t *)(adev->doorbell.ptr + index));
 #endif
@@ -508,8 +524,22 @@ void amdgpu_mm_wdoorbell64(struct amdgpu_device *adev, u32 index, u64 v)
 {
 	if (index < adev->doorbell.num_doorbells) {
 #ifdef __NetBSD__
+#ifdef _LP64
 		bus_space_write_8(adev->doorbell.bst, adev->doorbell.bsh,
 		    8*index, v);
+#else
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+		bus_space_write_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index, v & 0xffffffffU);
+		bus_space_write_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index + 4, v >> 32);
+#else
+		bus_space_write_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index, v >> 32);
+		bus_space_write_4(adev->doorbell.bst, adev->doorbell.bsh,
+		    8*index + 4, v & 0xffffffffU);
+#endif
+#endif
 #else
 		atomic64_set((atomic64_t *)(adev->doorbell.ptr + index), v);
 #endif