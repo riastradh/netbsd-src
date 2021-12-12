coypoop/drm

rockchip/drm: use an explicit task queue to avoid config_defer pitfalls.

sys/arch/arm/rockchip/rk_drm.c
@@ -82,6 +82,8 @@ static void	rk_drm_disable_vblank(struct drm_device *, unsigned int);
 static int	rk_drm_load(struct drm_device *, unsigned long);
 static void	rk_drm_unload(struct drm_device *);
 
+static void	rk_drm_task_work(struct work *, void *);
+
 static struct drm_driver rk_drm_driver = {
 	.driver_features = DRIVER_MODESET | DRIVER_GEM,
 	.dev_priv_size = 0,
@@ -131,6 +133,14 @@ rk_drm_attach(device_t parent, device_t self, void *aux)
 	sc->sc_dmat = faa->faa_dmat;
 	sc->sc_bst = faa->faa_bst;
 	sc->sc_phandle = faa->faa_phandle;
+	sc->sc_task_thread = NULL;
+	SIMPLEQ_INIT(&sc->sc_tasks);
+	if (workqueue_create(&sc->sc_task_wq, "rkdrm",
+	    &rk_drm_task_work, NULL, PRI_NONE, IPL_NONE, WQ_MPSAFE)) {
+		aprint_error_dev(self, "unable to create workqueue\n");
+		sc->sc_task_wq = NULL;
+		return;
+	}
 
 	aprint_naive("\n");
 
@@ -164,16 +174,41 @@ rk_drm_init(device_t dev)
 	struct drm_driver * const driver = &rk_drm_driver;
 	int error;
 
+	/*
+	 * Cause any tasks issued synchronously during attach to be
+	 * processed at the end of this function.
+	 */
+	sc->sc_task_thread = curlwp;
+
 	error = -drm_dev_register(sc->sc_ddev, 0);
 	if (error) {
 		aprint_error_dev(dev, "couldn't register DRM device: %d\n",
 		    error);
-		return;
+		goto out;
 	}
+	sc->sc_dev_registered = true;
 
 	aprint_normal_dev(dev, "initialized %s %d.%d.%d %s on minor %d\n",
 	    driver->name, driver->major, driver->minor, driver->patchlevel,
 	    driver->date, sc->sc_ddev->primary->index);
+
+	/*
+	 * Process asynchronous tasks queued synchronously during
+	 * attach.  This will be for display detection to attach a
+	 * framebuffer, so we have the opportunity for a console device
+	 * to attach before autoconf has completed, in time for init(8)
+	 * to find that console without panicking.
+	 */
+	while (!SIMPLEQ_EMPTY(&sc->sc_tasks)) {
+		struct rk_drm_task *const task =
+		SIMPLEQ_FIRST(&sc->sc_tasks);
+
+		SIMPLEQ_REMOVE_HEAD(&sc->sc_tasks, sdt_u.queue);
+		(*task->sdt_fn)(task);
+	}
+
+out:	/* Cause any subesquent tasks to be processed by the workqueue.  */
+	atomic_store_relaxed(&sc->sc_task_thread, NULL);
 }
 
 static vmem_t *
@@ -484,4 +519,32 @@ rk_drm_port_device(struct fdt_device_ports *port)
 			return sport->ddev;
 
 	return NULL;
+}
+
+static void
+rk_drm_task_work(struct work *work, void *cookie)
+{
+	struct rk_drm_task *task = container_of(work, struct rk_drm_task,
+	    sdt_u.work);
+
+	(*task->sdt_fn)(task);
+}
+
+void
+rk_task_init(struct rk_drm_task *task,
+    void (*fn)(struct rk_drm_task *))
+{
+
+	task->sdt_fn = fn;
+}
+
+void
+rk_task_schedule(device_t self, struct rk_drm_task *task)
+{
+	struct rk_drm_softc *sc = device_private(self);
+
+	if (atomic_load_relaxed(&sc->sc_task_thread) == curlwp)
+		SIMPLEQ_INSERT_TAIL(&sc->sc_tasks, task, sdt_u.queue);
+	else
+		workqueue_enqueue(sc->sc_task_wq, &task->sdt_u.work, NULL);
 }

riastradh/netbsd-src

sys/arch/arm/rockchip/rk_drm.c
@@ -82,6 +82,8 @@ static void	rk_drm_disable_vblank(struct drm_device *, unsigned int);
 static int	rk_drm_load(struct drm_device *, unsigned long);
 static void	rk_drm_unload(struct drm_device *);
 
+static void	rk_drm_task_work(struct work *, void *);
+
 static struct drm_driver rk_drm_driver = {
 	.driver_features = DRIVER_MODESET | DRIVER_GEM,
 	.dev_priv_size = 0,
@@ -131,6 +133,14 @@ rk_drm_attach(device_t parent, device_t self, void *aux)
 	sc->sc_dmat = faa->faa_dmat;
 	sc->sc_bst = faa->faa_bst;
 	sc->sc_phandle = faa->faa_phandle;
+	sc->sc_task_thread = NULL;
+	SIMPLEQ_INIT(&sc->sc_tasks);
+	if (workqueue_create(&sc->sc_task_wq, "rkdrm",
+	    &rk_drm_task_work, NULL, PRI_NONE, IPL_NONE, WQ_MPSAFE)) {
+		aprint_error_dev(self, "unable to create workqueue\n");
+		sc->sc_task_wq = NULL;
+		return;
+	}
 
 	aprint_naive("\n");
 
@@ -164,16 +174,40 @@ rk_drm_init(device_t dev)
 	struct drm_driver * const driver = &rk_drm_driver;
 	int error;
 
+	/*
+	 * Cause any tasks issued synchronously during attach to be
+	 * processed at the end of this function.
+	 */
+	sc->sc_task_thread = curlwp;
+
 	error = -drm_dev_register(sc->sc_ddev, 0);
 	if (error) {
 		aprint_error_dev(dev, "couldn't register DRM device: %d\n",
 		    error);
-		return;
+		goto out;
 	}
+	sc->sc_dev_registered = true;
 
 	aprint_normal_dev(dev, "initialized %s %d.%d.%d %s on minor %d\n",
 	    driver->name, driver->major, driver->minor, driver->patchlevel,
 	    driver->date, sc->sc_ddev->primary->index);
+
+	/*
+	 * Process asynchronous tasks queued synchronously during
+	 * attach.  This will be for display detection to attach a
+	 * framebuffer, so we have the opportunity for a console device
+	 * to attach before autoconf has completed, in time for init(8)
+	 * to find that console without panicking.
+	 */
+	while (!SIMPLEQ_EMPTY(&sc->sc_tasks)) {
+		struct rk_drm_task *const task = SIMPLEQ_FIRST(&sc->sc_tasks);
+
+		SIMPLEQ_REMOVE_HEAD(&sc->sc_tasks, rdt_u.queue);
+		(*task->rdt_fn)(task);
+	}
+
+out:	/* Cause any subesquent tasks to be processed by the workqueue.  */
+	atomic_store_relaxed(&sc->sc_task_thread, NULL);
 }
 
 static vmem_t *
@@ -483,4 +517,32 @@ rk_drm_port_device(struct fdt_device_ports *port)
 			return sport->ddev;
 
 	return NULL;
+}
+
+static void
+rk_drm_task_work(struct work *work, void *cookie)
+{
+	struct rk_drm_task *task = container_of(work, struct rk_drm_task,
+	    rdt_u.work);
+
+	(*task->rdt_fn)(task);
+}
+
+void
+rk_task_init(struct rk_drm_task *task,
+    void (*fn)(struct rk_drm_task *))
+{
+
+	task->rdt_fn = fn;
+}
+
+void
+rk_task_schedule(device_t self, struct rk_drm_task *task)
+{
+	struct rk_drm_softc *sc = device_private(self);
+
+	if (atomic_load_relaxed(&sc->sc_task_thread) == curlwp)
+		SIMPLEQ_INSERT_TAIL(&sc->sc_tasks, task, rdt_u.queue);
+	else
+		workqueue_enqueue(sc->sc_task_wq, &task->rdt_u.work, NULL);
 }